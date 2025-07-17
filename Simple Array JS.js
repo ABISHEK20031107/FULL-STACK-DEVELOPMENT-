5b69e2cc7102ba1e2844bd16ea7a50351c758a1e
ABISHEK20031107 <abishekindian008@gmail.com>
2025-07-17 10:31:15 +0530

    Add files via upload

---
+++ Simple Array JS.js
@@ -0,0 +1,13 @@
+const colors=["red","green","blue","yellow"];
+const numbers=[10,20,30,50];
+numbers.push(40);
+console.log(numbers[0]);
+
+console.log(colors[2]);
+
+  for(let i=0;i<numbers.length;i++)
+  {
+    console.log(numbers[i]);
+  }
+
+    console.log(colors.join(","));
