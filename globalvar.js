6a721f3d3fb6521bfc4f3c1c574eb14d6fcaa5ab
ABISHEK20031107 <abishekindian008@gmail.com>
2025-07-24 10:12:56 +0530

    Add files via upload

---
+++ globalvar.js
@@ -0,0 +1,11 @@
+let globalVar="i am global";
+function showGlobal()
+{
+    let name="Sandy";
+    console.log(globalVar);
+    console.log("HELLO"+name);
+
+}
+showGlobal();
+console.log(globalVar);
+console.log("HELLO")
