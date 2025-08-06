645fd4036644bafd3ecd07000f17098ea59631b4
ABISHEK20031107 <abishekindian008@gmail.com>
2025-08-06 17:12:14 +0530

    Add files via upload

---
+++ TASK 6 GET NAME.sql
@@ -0,0 +1,41 @@
+
+CREATE DATABASE company_db860;
+
+USE company_db860;
+
+CREATE TABLE departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
+
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    dept_id INT,
+    FOREIGN KEY (dept_id) REFERENCES departments(dept_id)
+);
+
+INSERT INTO departments (dept_id, dept_name) VALUES
+(1, 'HR'),
+(2, 'Finance'),
+(3, 'IT');
+
+INSERT INTO employees (emp_id, emp_name, dept_id) VALUES
+(101, 'Alice', 1),
+(102, 'Bob', 2),
+(103, 'Charlie', 3),
+(104, 'David', 1),
+(105, 'Eva', 2);
+
+SELECT 
+    e.emp_name,
+    d.dept_name
+FROM 
+    employees e
+INNER JOIN 
+    departments d
+ON 
+    e.dept_id = d.dept_id
+ORDER BY 
+    d.dept_name ASC;
