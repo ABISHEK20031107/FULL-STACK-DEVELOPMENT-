71ec157d01356909194739b82cc213d4bb93afe8
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-09 21:03:34 +0530

    Add files via upload

---
+++ SwitchExample.java
@@ -0,0 +1,24 @@
+package com.index;
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+
+		int month =4;
+		String monthString;
+		
+		switch (month)
+		{
+		case 1:monthString="january";break;
+		case 2:monthString="feb";break;
+		case 3:monthString="mar";break;
+		case 4:monthString="april";break;
+		case 5:monthString="may";break;
+		
+
+		}
+		
+		
+	}
+
+}
