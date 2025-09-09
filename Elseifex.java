80ab2585e51b646ff7128f3462a3f9d10d193fb6
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-09 20:25:31 +0530

    Add files via upload

---
+++ Elseifex.java
@@ -0,0 +1,104 @@
+package com.index;
+
+import java.util.Scanner;
+
+public class Elseifex {
+
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in);
+	    System.out.print("Enter the first number- ");
+	    
+		
+	    int a= sc.nextInt();
+	    System.out.print("enter the second number-");
+	    
+	    int b= sc.nextInt();
+	    System.out.print("enter the third number-");
+	    
+	    int c= sc.nextInt();
+	    int total=a+b+c;
+	    System.out.println("total ="+total);
+	    
+	    if (total > 100) {
+	        System.out.println("The value is greater than 100");
+	        System.out.println("The value is greater than 90");
+	        System.out.println("The value is greater than 80");
+	        System.out.println("The value is greater than 70");
+	        System.out.println("The value is greater than 60");
+	        System.out.println("The value is greater than 50");
+	        System.out.println("The value is greater than 40");
+	        System.out.println("The value is greater than 30");
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 90) {
+	    	    System.out.println("The value is greater than 90");
+	    	    System.out.println("The value is greater than 80");
+	    	    System.out.println("The value is greater than 70");
+	        System.out.println("The value is greater than 60");
+	        System.out.println("The value is greater than 50");
+	        System.out.println("The value is greater than 40");
+	        System.out.println("The value is greater than 30");
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 80) {
+	        System.out.println("The value is greater than 80");
+	        System.out.println("The value is greater than 70");
+	        System.out.println("The value is greater than 60");
+	        System.out.println("The value is greater than 50");
+	        System.out.println("The value is greater than 40");
+	        System.out.println("The value is greater than 30");
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 70) {
+	        System.out.println("The value is greater than 70");
+	        System.out.println("The value is greater than 60");
+	        System.out.println("The value is greater than 50");
+	        System.out.println("The value is greater than 40");
+	        System.out.println("The value is greater than 30");
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 60) {
+	        System.out.println("The value is greater than 60");
+	        System.out.println("The value is greater than 50");
+	        System.out.println("The value is greater than 40");
+	        System.out.println("The value is greater than 30");
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 50) {
+	        System.out.println("The value is greater than 50");
+	        System.out.println("The value is greater than 40");
+	        System.out.println("The value is greater than 30");
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 40) {
+	        System.out.println("The value is greater than 40");
+	        System.out.println("The value is greater than 30");
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 30) {
+	        System.out.println("The value is greater than 30");
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 20) {
+	        System.out.println("The value is greater than 20");
+	        System.out.println("The value is greater than 10");
+	    }
+	    else if (total > 10) {
+	        System.out.println("The value is greater than 10");
+	    }
+	 
+	    
+	
+	    
+	}
+
+}
