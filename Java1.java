cfa060210d32e2b8e9514012420e03f9d46c833e
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-09 21:02:09 +0530

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,35 @@
+package com.index;
+
+public class Java1 
+{
+	
+
+		 
+		public static void main(String[] args)
+		{
+			
+			int a=10;
+			float b=10.78f;
+			double c=1.78;
+			char d='r';
+			String e="sharuk";
+			long f=123456789;
+			boolean val=false;
+			short g=127;
+		
+		System.out.println(a);
+System.out.println(b);
+		System.out.println(c);
+	
+		int myInt=9;
+		double myDouble=myInt;
+		System.out.println("The values of myint is"+myInt);
+		System.out.println("The values of myDouble is"+myDouble);
+		
+		double myDouble1=9.78d;
+		
+		int myInt1=(int) myDouble;
+		}
+}
+
+		
