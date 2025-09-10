9f35432c74d6148252c855b2f57849ddd06afdfa
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-10 19:53:38 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,26 @@
+package com.index;
+
+public class Ternary {
+
+	public Ternary() {
+	
+	}
+
+	public static void main(String[] args) {
+		
+		int a=20,b=30;
+		
+		int max =(a>b)?a:b;
+		 
+		   
+		   System.out.println("max is :"+max);
+
+		   
+             int c=30,d=20;
+			int min =(c<d)?c:d;
+			 
+			   
+			   System.out.println("min is :"+min);
+	}
+
+}
