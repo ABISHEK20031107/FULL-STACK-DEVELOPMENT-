c25964358c765db2d8ff72527cd2dd5586022582
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-10 19:45:13 +0530

    Add files via upload

---
+++ ClassandObject.java
@@ -0,0 +1,25 @@
+package com.index;
+
+public class ClassandObject {
+	  String name ="Abishek P";
+	     int id=5;
+	     double salary=900000;
+	     int bonus=90000;
+
+	public static void main(String[] args) {
+     
+      	ClassandObject obj=new ClassandObject();
+      	ClassandObject obj1=new ClassandObject();
+      	
+      	System.out.println(obj.id);      	
+      	System.out.println(obj.name);
+      	System.out.println(obj1.salary);
+      	System.out.println(obj1.bonus);
+
+
+      	
+      		     
+     
+	}
+
+}
