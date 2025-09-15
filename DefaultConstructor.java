134061facd723ba02440a7348279887f5f6ea21c
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-15 19:23:53 +0530

    Add files via upload

---
+++ DefaultConstructor.java
@@ -0,0 +1,14 @@
+package com.index;
+
+public class DefaultConstructor {
+
+	DefaultConstructor(){
+		System.out.println("This is my Default constructor");
+	}
+	public static void main(String[] args) {
+		DefaultConstructor object= new DefaultConstructor();
+		// TODO Auto-generated method stub
+
+	}
+
+}
