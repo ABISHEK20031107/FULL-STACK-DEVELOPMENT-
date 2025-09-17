484ba1fb4bcc253b4101acd7fb8043b7994ad503
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-17 20:23:20 +0530

    Add files via upload

---
+++ ArrayImplemention.java
@@ -0,0 +1,27 @@
+package Array;
+import java.util.Scanner;
+public class ArrayImplemention {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		int n=sc.nextInt();
+		
+		int[] arr=new int[n];
+		
+		System.out.println("Enter "+n+"Element");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+			
+		}
+		
+		for(int i=0;i<n;i++) {
+			System.out.println("The Array Element Are ;");
+			System.out.println(arr[i]+"");
+		}
+		
+	}
+
+}
