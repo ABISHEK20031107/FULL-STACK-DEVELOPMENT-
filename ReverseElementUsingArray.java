aee0204d2e05aa7fe6babec5ffb3d93cce056fcb
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-22 18:40:45 +0530

    Update ReverseElementUsingArray.java

--- ReverseElementUsingArray.java
+++ ReverseElementUsingArray.java
@@ -7,14 +7,14 @@ public class ReverseElementUsingArray {
 		
 		Scanner sc=new Scanner(System.in);
 		
-		        // Take input
+		       
 		        System.out.print("Enter a number: ");
 		        String input = sc.nextLine();
 
-		        // Convert input string to array of characters
+		        
 		        char[] arr = input.toCharArray();
 
-		        // Print array in reverse order
+		      
 		        System.out.print("Reversed: ");
 		        for (int i = arr.length - 1; i >= 0; i--) {
 		            System.out.print(arr[i]);
@@ -23,5 +23,6 @@ public class ReverseElementUsingArray {
 		        sc.close();
 		    }
 		}
+
 
 
