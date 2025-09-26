5efafbd51138c52afd424c354f836be3c2acfe0a
ABISHEK20031107 <abishekindian008@gmail.com>
2025-09-26 09:51:07 +0530

    Add files via upload

---
+++ JaggedArrayOrRackedAray.java
@@ -0,0 +1,29 @@
+package Array;
+
+public class JaggedArrayOrRackedAray {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter the number of Arrays :");
+	
+		int [][] jag=new int [3][] ;
+		
+		
+		jag[0]=new int[] {1,2,3};
+		
+		jag [1]=new int [] {4,5};
+		
+		jag [2]=new int [] {6,7,8,9};
+		
+		for (int i=0;i<jag.length;i++) {
+
+			for (int j=0;j<jag[i].length;j++) {
+				
+				System.out.print(jag[i][j]+" ");
+				
+				System.out.println();
+			}
+		}
+	}
+
+}
