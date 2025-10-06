b25e535b3f156fcca4ffd52c433bff1c8947d5af
ABISHEK20031107 <abishekindian008@gmail.com>
2025-10-06 21:42:22 +0530

    Add files via upload

--- IncreaseBy3.java
+++ IncreaseBy3.java
@@ -4,15 +4,19 @@ public class IncreaseBy3 {
 
 	public static void main(String[] args) {
 
-		int num=12;
-		for(int i=1;i<=5;i++) {
+		int num=1;
+		
+		for(int i=1;i<=9;i++) //ROW
+		{
 			
-			for(int j=1;j<=i;j++) {
+			for(int j=1;j<=i;j++)//COLUMN
+		{
 				
 				System.out.print(num +" ");
-				num+=3;
+				num+=1;
 				
 			}
+			
 			System.out.println();
 		}
 	}
