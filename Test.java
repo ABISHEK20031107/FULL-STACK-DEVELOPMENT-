41ce23def066a7f774cf98b316221d45265530d1
ABISHEK20031107 <abishekindian008@gmail.com>
2025-10-09 10:02:35 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package com.index;
+
+public class Test {
+	int employee_id;
+	String employee_name;
+
+	public static void main(String[] args) {
+
+		Test tes=new Test();
+		tes.employee_id=123;
+		tes.employee_name="Abi";
+		System.out.println(tes.employee_id);
+	}
+
+}
